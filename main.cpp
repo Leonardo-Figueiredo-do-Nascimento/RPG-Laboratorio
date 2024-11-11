005c0598411c6785ce81f0c359e026eae7fbe069
LEONARDO FIGUEIREDO DO NASCIMENTO <kriptonascimento@hotmail.com>
2024-11-11 11:16:14 -0300

    Criação do menu

--- main.cpp
+++ main.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include "menu/menu.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	int opcao = Menu();
+	
+	while(opcao != 1 || opcao != 2 || opcao != 3){
+		if (opcao == 1) {
+			
+			int personagem;
+			
+			while(personagem != 1 && personagem != 2 && personagem != 3){
+				
+				std::cout << "Escolha um personagem:" << std::endl;
+		        std::cout << "\n[1] Barbaro     Vida:      Ataque:      Pocoes de cura: 1" << std::endl;
+		        std::cout << "[2] Arqueira    Vida:      Ataque:      Pocoes de cura: 2" << std::endl;
+		        std::cout << "[3] Mago        Vida:      Ataque:      Pocoes de cura: 3" << std::endl;
+		        
+				std::cout << "\n-> ";
+		    	std::cin >> personagem;
+		        
+		        if(personagem == 1){
+		        	
+				}
+				else if(personagem == 2){
+					
+				}
+				else if(personagem == 3){
+				
+				}
+				else{
+					std::cout << "\nOpcao invalida"<<std::endl;
+				}
+			} 
+		}
+	        
+		else if (opcao == 2) {
+			
+	        std::cout << "Carregando jogo salvo..." << std::endl;
+	    
+		} 
+		else if (opcao == 3) {
+	        exit(0);
+	    
+		} else {
+	        std::cout << "Opcao invalida." << std::endl;
+	    }
+	}
 	return 0;
 }
---
+++ menu/menu.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+int Menu() {
+    int escolha;
+
+    std::ifstream arquivo("save/save.txt");
+
+    std::cout << "[1] Iniciar novo jogo" << std::endl;
+
+    if (arquivo) {
+        std::cout << "[2] Carregar jogo" << std::endl;
+    }
+
+    std::cout << "[3] Encerrar jogo" << std::endl;
+    std::cout << "-> ";
+    std::cin >> escolha;
+
+    arquivo.close();
+
+    return escolha;
+}
