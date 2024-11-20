4d39822472635bd45e0885bcf81c45bbaefee2d1
LuisM <luismiguel032006@gmail.com>
2024-11-20 20:48:52 -0300

    Create Barbaro.h

---
+++ personagens/Barbaro.h
@@ -0,0 +1,24 @@
+#include "Protagonista.h"
+#include <string>
+#include <iostream>
+
+struct Barbaro : public Protagonista {
+    Barbaro(){
+    vida = 100;            // Vida inicial
+        vidaMax = 100;         // Vida máxima
+        danoAtq = 20;          // Dano de ataque
+        classe = "Barbaro";  // Nome da classe
+        pocoesCura[0] = 1; // Poção disponível
+        pocoesCura[1] = 0; // Poção indisponível
+        pocoesCura[2] = 0; // Poção indisponível
+    }
+
+    void ataqueDeMagia(){
+            std::cout<<"seu ataque causou 20 de dano\n";
+        };
+
+        void contarHistoria(){
+            std::cout<<"Ragnar, um bárbaro das terras do Norte, teve um sonho enigmático sobre o Olho de Skarn, um artefato lendário que controlava o destino das terras selvagens. Determinado, ele enfrentou perigos mortais — feras, trolls e frio implacável — até chegar ao templo onde o artefato estava guardado. Lá, superou o enigma de uma esfinge e enfrentou o espírito de um antigo guerreiro em combate. No momento decisivo, Ragnar provou sua honra ao abdicar da violência, demonstrando que sua busca era pelo bem de seu povo. Ele conquistou o Olho de Skarn e retornou como líder, trazendo prosperidade e deixando um legado de coragem e sabedoria.\n";
+        };
+
+};
